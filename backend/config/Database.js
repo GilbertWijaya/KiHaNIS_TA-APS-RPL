d27e878a7cb424d2df626dab655099b0643d2d74
ACER ASPIRE <yhsgilbertyhs@gmail.com>
2024-12-09 17:47:14 +0700

    test

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -3,7 +3,8 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize("kihanis_db", process.env.DATABASE_USER, process.env.DATABASE_PASS,{
 
     host : "localhost",
-    dialect : "mysql"
+    dialect : "mysql",
+    dialectModule : require('mysql2')
 
 });
 
